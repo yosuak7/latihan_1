7cd3eebb865cd6526f1a22f2ecca9e8151b3cf7c
yosuak7 <101914056+yosuak7@users.noreply.github.com>
2022-03-26 11:16:45 +0700

    Add files via upload

---
+++ model_latihan1.php
@@ -0,0 +1,14 @@
+<?php
+class model_latihan1 extends CI_model
+{
+    public $nilai1, $nilai2, $hasil;
+
+    public function jumlah($nil1 = null, $nil2 = null)
+    {
+    $this->nilai1 = $nil1;
+    $this->nilai2 = $nil2;
+    $this->hasil = $this->nilai1 + $this->nilai2;
+    return $this->hasil;
+    }
+
+}
