3a94c47b297c73905e6d9f61165ad0adc20bd678
yosuak7 <101914056+yosuak7@users.noreply.github.com>
2022-03-26 11:17:46 +0700

    Add files via upload

---
+++ contoh3.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+    </head>
+
+    <body>
+        <b> halo guys.. masih belajar ini kita :D <br>
+        Nilai 1 = <?= $nilai1; ?>   <br>
+        Nilai 2 = <?= $nilai2; ?>   <br>
+
+        ini hasil dari permodelan dengan methode penjumahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+    </body>
+</html>
