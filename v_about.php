3ec6d5f2f6c097fd6e229d6451bdca32355ad63c
yosuak7 <101914056+yosuak7@users.noreply.github.com>
2022-04-05 20:51:11 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+        <li>Nama Depan : Yosua</li>
+        <li>Nama Belakang : Koesnanto</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+        <li> Perum Taman Walet Pasar kemis</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+        <li>Ngawi</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+ </ul>
+</section>
